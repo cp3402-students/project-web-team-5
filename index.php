be886e066b148ae2a35fc8b3d833b3d0c6e0c09f
Paul <paul.guirguis@my.jcu.edu.au>
2023-05-10 18:33:11 +1000

    test commit 2

--- index.php
+++ index.php
@@ -35,12 +35,55 @@ $container = get_theme_mod( 'understrap_container_type' );
 			?>
 
 			<main class="site-main" id="main">
+			<?php $Credo = array(
+				// Create the query loop to show the the websites credo
+				'post_type' => 'post' ,
+				'orderby' => 'date' ,
+				'order' => 'DESC' ,
+				'posts_per_page' => 6,
+				'category_name' => 'credo',
+				'paged' => get_query_var('paged'),
+				'post_parent' => $parent
+			); ?>
 
-				<?php
-				if ( have_posts() ) {
+			<?php
+			// Shows the websites credo
+				$CredoQuery = new WP_Query($Credo);
+				if ( $CredoQuery->have_posts() ) {
 					// Start the Loop.
-					while ( have_posts() ) {
-						the_post();
+					while ( $CredoQuery->have_posts() ) {
+						$CredoQuery->the_post();
+						
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'loop-templates/content', get_post_format() );
+					}
+				} else {
+					get_template_part( 'loop-templates/content', 'none' );
+				}
+			?>
+
+			<?php $Committee = array(
+				// Create the query loop to show the the websites committee memebers 
+				'post_type' => 'post' ,
+				'orderby' => 'date' ,
+				'order' => 'DESC' ,
+				'posts_per_page' => 6,
+				'category_name' => 'committee',
+				'paged' => get_query_var('paged'),
+				'post_parent' => $parent
+			); ?>
+
+			<?php
+				$CommitteeQuery = new WP_Query($Committee);
+				// Shows the websites committee memebers 
+				if ( $CommitteeQuery->have_posts() ) {
+					// Start the Loop.
+					while ( $CommitteeQuery->have_posts() ) {
+						$CommitteeQuery->the_post();
 
 						/*
 						 * Include the Post-Format-specific template for the content.
@@ -52,7 +95,73 @@ $container = get_theme_mod( 'understrap_container_type' );
 				} else {
 					get_template_part( 'loop-templates/content', 'none' );
 				}
-				?>
+			?>
+
+			<?php $Sponsors = array(
+				// Create the query loop to show the the websites Sponsors
+				'post_type' => 'post' ,
+				'orderby' => 'date' ,
+				'order' => 'DESC' ,
+				'posts_per_page' => 6,
+				'category_name' => 'sponsors',
+				'paged' => get_query_var('paged'),
+				'post_parent' => $parent
+			); ?>
+
+			<?php
+				echo ("Sponsers:");
+				// Shows the websites Sponsors
+				$SponsorQuery = new WP_Query($Sponsors);
+				if ( $SponsorQuery->have_posts() ) {
+					// Start the Loop.
+					while ( $SponsorQuery->have_posts() ) {
+						$SponsorQuery->the_post();
+
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'loop-templates/content', get_post_format() );
+					}
+				} else {
+					get_template_part( 'loop-templates/content', 'none' );
+				}
+			?>
+
+			<?php $RadioStations = array(
+				// Create the query loop to show the the websites Radio Sponsors
+				'post_type' => 'post' ,
+				'orderby' => 'date' ,
+				'order' => 'ASC' ,
+				'posts_per_page' => 6,
+				'category_name' => 'RadioSponsors',
+				'paged' => get_query_var('paged'),
+				'post_parent' => $parent
+			); ?>
+
+			<?php
+				echo ("Country Music Radio Stations that are Valued
+				Supporters of The Townsville and Thuringowa
+				Country Music Assn.");
+				// Shows the websites Radio Sponsors
+				$RadioQuery = new WP_Query($RadioStations);
+				if ( $RadioQuery->have_posts() ) {
+					// Start the Loop.
+					while ( $RadioQuery->have_posts() ) {
+						$RadioQuery->the_post();
+
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'loop-templates/content', get_post_format() );
+					}
+				} else {
+					get_template_part( 'loop-templates/content', 'none' );
+				}
+			?>
 
 			</main>
 
